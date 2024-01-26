6d5a4c65e740ad647f462396fb929b2ca628a596
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2024-01-26 09:54:10 +0500

    I remove extra text

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -48,7 +48,7 @@ const Form = () => {
                     <form onSubmit={(e) => e.preventDefault()}>
                         <div className="Mydivform">
                             <div className="heading-work">
-                                <h1>To-do List Here</h1>
+                                <h1>To-do List</h1>
                             </div>
                             {/* Step 4 */}
                             <div className="mybox">
